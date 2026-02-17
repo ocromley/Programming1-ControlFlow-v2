f30905673f83a6bb36ce7fe1e525b81992de00b1
ocromley <ocromley@icstars.org>
2026-02-17 12:22:51 -0600

    scaffolding the structure of the solution to the conditions.js kata

--- js-katas/katas/conditions.js
+++ js-katas/katas/conditions.js
@@ -3,10 +3,29 @@
 function fizzBuzz(n = 100) {
   // TODO: write your solution here (replace the line below).
   // Return an array of values for 1..n following FizzBuzz rules.
-  // Example: fizzBuzz(5) -> [1, 2, "Fizz", 4, "Buzz"]
-  // Explain your logic choices in comments as you code.
-  throw new Error('Not implemented');
-}
+  // Example: fizzBuzz(5) -> [1, 2, "Fizz", 4, "Buzz", "Fizz",7,8, fizz,buzz...14, fizzbuzz]
+  // Explain your logic chothrow new Error('Not implemented');}
+  // Purpose: return an array
+ //n is the limit to the amount of times that this loop will loop 
+
+  for (let i = 1; i <=20; i++){
+    if(i % 15===0){
+      console.log("fizzbuzz")
+    }
+    if(i % 3===0){
+    console.log("fizz");
+  }else if (i % 3 ===0){
+    console.log("buzz");
+  }else if(i % 5===0){
+    console.log(buzz);
+  }else {
+    console.log(i)
+    }
+  }
+  
+  
+ }
+
 
 globalThis.__katas = globalThis.__katas || {};
 globalThis.__katas.fizzBuzz = fizzBuzz;
