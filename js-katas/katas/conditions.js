110a2f7930b8038467ea4521b344b8bc2f11954e
ocromley <ocromley@icstars.org>
2026-02-17 13:12:37 -0600

    scafolding the structure of the solution to the condition.js kata

--- js-katas/katas/conditions.js
+++ js-katas/katas/conditions.js
@@ -10,22 +10,20 @@ function fizzBuzz(n = 100) {
 
   for (let i = 1; i <=20; i++){
     if(i % 15===0){
-      console.log("fizzbuzz")
-    }
-    if(i % 3===0){
-    console.log("fizz");
-  }else if (i % 3 ===0){
-    console.log("buzz");
-  }else if(i % 5===0){
-    console.log(buzz);
-  }else {
-    console.log(i)
+      console.log("fizzbuzz")}
+    else if(i % 3===0){
+      console.log("fizz");}
+    else if (i % 5 ===0){
+      console.log("buzz");}
+    else {
+      console.log(i)
     }
   }
-  
-  
  }
 
+fizzBuzz(5);
 
 globalThis.__katas = globalThis.__katas || {};
 globalThis.__katas.fizzBuzz = fizzBuzz;
+
+
